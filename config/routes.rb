ae43f6fc4a20da429b7c62ff664054e62cc34239
marmo002 <martin.st8n@gmail.com>
2020-10-20 17:52:44 -0400

    PagesCOntroller: removed action show, its code, private method and replaced it with home action. Removed code snippet from application layout, fixed routes to match changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "pages#show", page: "home"
+  root "pages#home"
 
-  get "/pages/:page" => "pages#show"
+  get "/pages/home" => "pages#home"
 
 end
