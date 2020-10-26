6111ff8341091d673b45b1714692b679834cb3ae
marmo002 <martin.st8n@gmail.com>
2020-10-26 15:35:27 -0400

    added active storage config to brand

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,6 +2,8 @@ class Brand < ApplicationRecord
   validates :name,     presence: { message: "Nombre no puede estar vacio" }
   validates :name,     uniqueness: { case_sensitive: false, message: "Marca ya existe!" }
 
+  has_one_attached :logo
+
   def name=(name)
     write_attribute(:name, name.strip.downcase.titleize)
   end
