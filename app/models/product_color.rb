9ccf54f854c12c540d26851d2540a8d929a64072
marmo002 <martin.st8n@gmail.com>
2020-11-07 12:49:28 -0500

    Added dependent destroy to product_dolor associtation with color_sizes

--- app/models/product_color.rb
+++ app/models/product_color.rb
@@ -2,7 +2,7 @@ class ProductColor < ApplicationRecord
   belongs_to :product
   belongs_to :color
 
-  has_many :color_sizes
+  has_many :color_sizes, dependent: :destroy
   has_many :sizes, through: :color_sizes
 
   accepts_nested_attributes_for :color_sizes, allow_destroy: true, reject_if: :all_blank
