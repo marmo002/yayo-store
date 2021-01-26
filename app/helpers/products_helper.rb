8ff6f77ceec55c6f6b4666d7dee362b17cdd3ff1
marmo002 <martin.st8n@gmail.com>
2021-01-25 20:44:41 -0500

    pages_controller:got categories, brands, and used find_by_sql to get products with status=active and stock > 0. products_helper: added number_to_soles to return the price from number to currency and products_title to return category and brand if title is empty. product_color and color_size: added belongs_to ...touch: true so product is touch when this items are saved/updated. products/shop: iterate over products and displaey basic info

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+    def number_to_soles(number_to_soles)
+        number_to_currency(number_to_soles, unit: 'S/.')
+    end
+
+    def product_title(product)
+        product.title.empty? ? "#{product.category} #{product.marca}" : product.title
+    end
 end
