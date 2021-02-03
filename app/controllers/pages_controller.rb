7ec086a27f48e56e2784bc3e5f54ac01e7041958
marmo002 <martin.st8n@gmail.com>
2021-02-02 20:32:22 -0500

    Modified pages_controller#shop sql to gather the first product_color image blob id or nil. Shop view added logic to get product_color image temp url or display a image placeholder if no image available. Added css for fixed width and height of product image

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,6 +16,7 @@ class PagesController < ApplicationController
           products.sale_price,
           types.name as category,
           brands.name as marca,
+          attachments.blob_id as blob,
           SUM(color_sizes.stock) as stock
         FROM products
         INNER JOIN types
@@ -26,6 +27,18 @@ class PagesController < ApplicationController
           ON products.id = product_colors.product_id
         LEFT JOIN color_sizes
           ON product_colors.id = color_sizes.product_color_id
+        left join (
+            SELECT
+            p_color.id as product_color_id,
+            min(sb.id) as blob_id
+            FROM product_colors p_color
+            INNER JOIN active_storage_attachments sa
+            ON p_color.id = sa.record_id
+            INNER JOIN active_storage_blobs sb
+            ON sa.blob_id = sb.id
+            GROUP BY product_color_id
+          ) as attachments
+          ON product_colors.id = attachments.product_color_id
         WHERE status = 1
         GROUP BY
           products.id,
@@ -33,7 +46,8 @@ class PagesController < ApplicationController
           marca,
           products.title,
           products.price,
-          products.sale_price
+          products.sale_price,
+          attachments.blob_id
         HAVING SUM(color_sizes.stock) > 0
         ORDER BY products.updated_at desc"
     )
